52c8bd817b0c0fbceed7decf2b9442c9d3ac3a22
Sumit9819 <44696990+Sumit9819@users.noreply.github.com>
2025-08-28 22:07:12 +0545

    Update [video_id].js

--- api/[video_id].js
+++ api/[video_id].js
@@ -8,7 +8,6 @@ const videoData = {
         "title": "Backshot 10035",
         "description": "Get the 10035 backshot.",
     },
-
 };
 
 export default async function handler(req, res) {
@@ -19,29 +18,29 @@ export default async function handler(req, res) {
     }
 
     const data = videoData[video_id];
-    const playerUrl = `https://${req.headers.host}/${video_id}`;
+    const playerUrl = `https://${req.headers.host}/player/${video_id}`;
     const redirectUrl = data.redirect_url;
 
     const htmlContent = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="0; url=${redirectUrl}">
-        <title>${data.title}</title>
-        <meta name="twitter:card" content="player">
-        <meta name="twitter:site" content="@your_twitter_handle">
-        <meta name="twitter:title" content="${data.title}">
-        <meta name="twitter:description" content="${data.description}">
-        <meta name="twitter:player" content="${data.video_path}">
-        <meta name="twitter:player:width" content="1280">
-        <meta name="twitter:player:height" content="720">
-    </head>
-    <body>
-        <p>If you are not redirected, please <a href="${redirectUrl}">click here</a>.</p>
-    </body>
-    </html>
-`;
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="refresh" content="0; url=${redirectUrl}">
+            <title>${data.title}</title>
+            <meta name="twitter:card" content="player">
+            <meta name="twitter:site" content="@your_twitter_handle">
+            <meta name="twitter:title" content="${data.title}">
+            <meta name="twitter:description" content="${data.description}">
+            <meta name="twitter:player" content="${playerUrl}">
+            <meta name="twitter:player:width" content="1280">
+            <meta name="twitter:player:height" content="720">
+        </head>
+        <body>
+            <p>If you are not redirected, please <a href="${redirectUrl}">click here</a>.</p>
+        </body>
+        </html>
+    `;
 
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send(htmlContent);
